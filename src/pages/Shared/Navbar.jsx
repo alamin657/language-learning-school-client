1b8e94350519bee0a6451e2cae989332de2caa64
Alamin <alamin017514@gmail.com>
2023-06-07 08:54:06 +0600

    added navbar section

--- src/pages/Shared/Navbar.jsx
+++ src/pages/Shared/Navbar.jsx
@@ -1,9 +1,35 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
+import logo from '../../assets/logo/25496642.jpg'
 const Navbar = () => {
+    const learningOptions = <>
+        <li><Link>Home</Link></li>
+        <li><Link>Instructors</Link></li>
+        <li><Link>Classes</Link></li>
+        <li><Link>DashBoard</Link></li>
+    </>
     return (
-        <div>
-            <h1>this is al jamijjgdjgjltuljjg</h1>
+        <div className="navbar bg-opacity-20 bg-black  max-w-screen-xl  ">
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                    </label>
+                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                        {learningOptions}
+                    </ul>
+                </div>
+                <img className='w-12 h-12 rounded-full' src={logo} alt="" />
+                <a className="btn btn-ghost text-xl">Language Learning</a>
+            </div>
+            <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal px-1">
+                    {learningOptions}
+                </ul>
+            </div>
+            <div className="navbar-end">
+                <a>picture</a>
+            </div>
         </div>
     );
 };
